c4b00515f41564c8e834c2964825f50fb18ef098
Archit Jain <architjain2005@gmail.com>
2026-07-22 01:02:54 +0530

    Added footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -4,11 +4,12 @@ import Header from './Header.jsx'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Biodata from './Biodata.jsx'
+import Footer from './Footer.jsx'
 
 createRoot(document.getElementById('root')).render(
   <>
     <Header />
     <Biodata />
-    <Biodata />
+    <Footer />
   </>,
 )
---
+++ src/components/Footer.jsx
@@ -0,0 +1,22 @@
+import '../style/Footer.css'
+
+function Footer() {
+    return(
+<div className='Footer-body'>
+<div><ul>
+    <li>one</li>
+    <li>one</li>
+    <li>one</li>
+    <li>one</li>
+    <li>one</li>
+    <li>one</li>
+    <li>one</li>
+    </ul>
+    </div>
+<div></div>
+<div></div>
+</div>
+    )
+}
+
+export default Footer
