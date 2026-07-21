94719c5436350edb1a1e18464325bd6921bd1ddb
Archit Jain <architjain2005@gmail.com>
2026-07-21 19:56:09 +0530

    added favicon

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import '../style/header.jsx';
+
+function Header() {
+    return (
+        <div className="Hedar-main">
+
+        </div>
+    )
+}
+
+export default Header
