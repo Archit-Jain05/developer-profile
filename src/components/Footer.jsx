181e0ed963ac6574528f0f4ed2fb51bee17d3cce
Archit Jain <architjain2005@gmail.com>
2026-07-22 20:08:54 +0530

    Fixed reactive web sizing

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,39 +8,41 @@ function Footer() {
         <div className='img-holder'>
             <img src={PFP} style={{width:"10vw"}} />
         </div>
-        <div>
+        <div className='ulist'>
             <ul className='montserrat-li'>
-                <li>Education</li>
-                <li>Experience</li>
-                <li>About Me</li>
-                <li>Contact Me</li>
+                <li><a href="">Education</a></li>
+                <li><a href="">Experience</a></li>
+                <li><a href="">About Me</a></li>
+                <li><a href="">Contact Me</a></li>
             </ul>
         </div>
-        <div>
+        <div className='ulist'>
             <ul className='montserrat-li'>
-                <li>Education</li>
-                <li>Experience</li>
-                <li>About Me</li>
-                <li>Contact Me</li>
+                <li><a href="">Projects</a></li>
+                <li><a href="">Services</a></li>
+                <li><a href="">Resume</a></li>
+                <li><a href=""></a></li>
                 </ul>
         </div>
         <div className='form'>
+            
+                <h1 className='montserrat-h1-form' style={{paddingLeft:"3vw", marginBottom:"0", margintop:"2vh  "}}>Reach out to me :-</h1>
+            
              <div>
-                <h1 className='montserrat-h1-form'>Reach out to me:</h1>
-             </div>
-             <div>
-                <input type="text" style={{height:"3vh"}}/>
+                <input type="text" style={{height:"5vh" }} placeholder='Enter your email id' className='montserrat-m'/>
                 
              </div>
+
              <div>
-                <input type="text" style={{height:"10vh"}} />
+                <textarea id="message" name="message" className='montserrat-m' style={{height:"15vh",alignContent:"center"}} placeholder='Type your content'></textarea>
              </div>
-             <div>
-                <button></button>
-             </div>
-            
+
+            <button><p className='montserrat-s'>Send</p></button>
         </div>
 
+    </div>
+    <div className='bottom-credit'>
+        <p className='montserrat-s'>Created by Archit Jain.</p>
     </div>
 </div>
     )
