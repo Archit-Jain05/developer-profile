54499ed8c7cd3ea6857531af55bbf57764be2519
Archit Jain <architjain2005@gmail.com>
2026-07-22 17:37:13 +0530

    Added footer form

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,20 +1,47 @@
 import '../style/Footer.css'
+import PFP from '../assets/pfp.svg'
 
 function Footer() {
     return(
 <div className='Footer-body'>
-<div><ul>
-    <li>one</li>
-    <li>one</li>
-    <li>one</li>
-    <li>one</li>
-    <li>one</li>
-    <li>one</li>
-    <li>one</li>
-    </ul>
+    <div className='Footer-card'>
+        <div className='img-holder'>
+            <img src={PFP} style={{width:"10vw"}} />
+        </div>
+        <div>
+            <ul className='montserrat-li'>
+                <li>Education</li>
+                <li>Experience</li>
+                <li>About Me</li>
+                <li>Contact Me</li>
+            </ul>
+        </div>
+        <div>
+            <ul className='montserrat-li'>
+                <li>Education</li>
+                <li>Experience</li>
+                <li>About Me</li>
+                <li>Contact Me</li>
+                </ul>
+        </div>
+        <div className='form'>
+             <div>
+                <h1 className='montserrat-h1-form'>Reach out to me:</h1>
+             </div>
+             <div>
+                <input type="text" style={{height:"3vh"}}/>
+                
+             </div>
+             <div>
+                <input type="text" style={{height:"10vh"}} />
+             </div>
+             <div>
+                <button></button>
+             </div>
+            
+        </div>
+
     </div>
-<div></div>
-<div></div>
 </div>
     )
 }
